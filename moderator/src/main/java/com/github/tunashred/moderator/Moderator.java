a71ca4bdadd35b35e37960579a6e4a0272f34008
tunashred <enachealexandru.traian@gmail.com>
2025-02-09 15:47:00 +0200

    Initial state after refactor

---
+++ moderator/src/main/java/com/github/tunashred/moderator/Moderator.java
@@ -0,0 +1,49 @@
+package com.github.tunashred.moderator;
+
+import com.github.tunashred.messageformats.MessageInfo;
+import com.github.tunashred.messageformats.ProcessedMessage;
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Moderator {
+    private Trie bannedWords;
+
+    public Moderator(String file_path) {
+        Trie.TrieBuilder trieBuilder = Trie.builder().ignoreCase();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file_path));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                trieBuilder.addKeyword(line.trim().toLowerCase());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.bannedWords = trieBuilder.build();
+    }
+
+    public static void main(String[] args) {
+        Moderator moderator = new Moderator("packs/banned.txt");
+        MessageInfo messageInfo = new MessageInfo(null, null, "");
+        ProcessedMessage output = moderator.censor(messageInfo);
+        System.out.println(output.getProcessedMessage());
+    }
+
+    public ProcessedMessage censor(MessageInfo messageInfo) {
+        String message = messageInfo.getMessage();
+        StringBuilder censoredMessage = new StringBuilder(message);
+        boolean isCensored = false;
+
+        for (Emit emit : bannedWords.parseText(message)) {
+            int start = emit.getStart(), end = emit.getEnd();
+            String replacement = "*".repeat(emit.getKeyword().length());
+            censoredMessage.replace(start, end + 1, replacement);
+            isCensored = true;
+        }
+        return new ProcessedMessage(messageInfo, censoredMessage.toString(), isCensored);
+    }
+}
---
+++ moderator/src/main/java/com/github/tunashred/moderator/ModeratorProducerConsumer.java
@@ -0,0 +1,74 @@
+package com.github.tunashred.moderator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tunashred.messageformats.MessageInfo;
+import com.github.tunashred.messageformats.ProcessedMessage;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class ModeratorProducerConsumer {
+    public static void main(String[] args) {
+        Properties consumerProps = new Properties();
+        consumerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        consumerProps.put(GROUP_ID_CONFIG, "moderator-consumer-group");
+        consumerProps.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProps.put("acks", "all");
+
+        Properties producerProps = new Properties();
+        producerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        producerProps.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+        consumer.subscribe(Collections.singletonList("unsafe_chat"));
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
+
+        Moderator moderator = new Moderator("packs/banned.txt");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : consumerRecords) {
+                    MessageInfo messageInfo = objectMapper.readValue(record.value(), MessageInfo.class);
+
+                    ProcessedMessage processedMessage = moderator.censor(messageInfo);
+                    System.out.println("\nGroup chat: " + messageInfo.getGroupChat().getChatName() + "/" + messageInfo.getGroupChat().getChatID() +
+                            "\nmessage.User: " + messageInfo.getUser().getName() + "/" + messageInfo.getUser().getUserID() +
+                            "\nOriginal message: " + messageInfo.getMessage() + "\nProcessed message: " + processedMessage.getProcessedMessage());
+
+                    if (processedMessage.isCensored()) {
+                        producer.send(new ProducerRecord<>("flagged_messages", record.key(), record.value()));
+                    }
+                    // here should send the thing serialized
+                    producer.send(new ProducerRecord<>("safe_chat", record.key(), processedMessage.getProcessedMessage()));
+                    consumer.commitSync();
+                }
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            consumer.close();
+            producer.close();
+        }
+    }
+}
