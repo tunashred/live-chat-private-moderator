1cc24ea927640a1b05c005295e23afc690f77f67
tunashred <enachealexandru.traian@gmail.com>
2025-02-22 20:00:51 +0200

    Move properties to external file

--- live-chat-private-moderator/src/main/java/com/github/tunashred/moderator/ModeratorProducerConsumer.java
+++ live-chat-private-moderator/src/main/java/com/github/tunashred/moderator/ModeratorProducerConsumer.java
@@ -3,25 +3,28 @@ package com.github.tunashred.moderator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.tunashred.dtos.MessageInfo;
 import com.github.tunashred.privatedtos.ProcessedMessage;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
-
-import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.streams.StreamsConfig.*;
 
 public class ModeratorProducerConsumer {
     public static void main(String[] args) {
         Properties streamsProps = new Properties();
-        streamsProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
-        streamsProps.put(APPLICATION_ID_CONFIG, "moderator-application");
-        streamsProps.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsProps.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        try (InputStream propsFile = new FileInputStream("src/main/resources/moderator_streams.properties")) {
+            streamsProps.load(propsFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // maybe add instead some default properties? but then what is the purpose of using an externalized config
+            // if not for the fewer lines of code in this file?
+            throw new RuntimeException(e.getMessage());
+        }
 
         StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> inputStream = builder.stream("unsafe_chat");
