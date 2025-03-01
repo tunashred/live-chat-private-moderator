010e393e2f5132ca7ce3d19c96b9e0c786dafad0
tunashred <enachealexandru.traian@gmail.com>
2025-03-01 17:15:52 +0200

    Refactor BannedWordsManager class

--- live-chat-private-moderator/src/main/java/com/github/tunashred/utils/BannedWordsManager.java
+++ live-chat-private-moderator/src/main/java/com/github/tunashred/utils/BannedWordsManager.java
@@ -1,24 +1,16 @@
 package com.github.tunashred.utils;
 
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.io.*;
-import java.time.Duration;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
+// TODO: add more checks for edge cases
 public class BannedWordsManager {
     public static void main(String[] args) throws IOException {
-        Properties consumerProps = new Properties();
-        try (InputStream propsFile = new FileInputStream("src/main/resources/consumer.properties")) {
-            consumerProps.load(propsFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
         Properties producerProps = new Properties();
         try (InputStream propsFile = new FileInputStream("src/main/resources/producer.properties")) {
             producerProps.load(propsFile);
@@ -27,16 +19,18 @@ public class BannedWordsManager {
             throw new RuntimeException(e.getMessage());
         }
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Add compacted topic name:");
         String input = reader.readLine().trim();
 
         // since anyone can create topics if they do not exist, I need to make sure this won't happen accidentally
-        if ("banned-words".equals(input)) {
-            consumer.subscribe(Collections.singletonList(input));
-        } else {
-            System.out.println("Invalid topic name. Exiting...");
+        // this could be a barbaric way to add valid topics for the operations done by this class
+        // though, it might not matter so much since this would be a private tool
+        List<String> topics = new ArrayList<>();
+        topics.add("banned-words");
+
+        if (!topics.contains(input)) {
+            System.out.println("Invalid topic name!");
             return;
         }
         final String topicName = input;
@@ -53,32 +47,26 @@ public class BannedWordsManager {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
 
+        // TODO: I could maybe have an option to give a list?
         if (option == 1) { // add word logic
             System.out.println("Word to add to " + topicName + " topic: ");
-            input = reader.readLine();
-            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, input, input);
-            producer.send(record);
-            System.out.println("Words added!");
+            String word = reader.readLine().trim();
+
+            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, word, word);
+            producer.send(record, (recordMetadata, e) -> {
+                if (e != null) {
+                    System.out.println("Error while trying to add word: " + e.getMessage());
+                } else {
+                    System.out.println("Word added!");
+                }
+            });
         } else if (option == 2) { // remove word logic
-            // first I need to check if the record exists inside the topic
-            // actually this means I need the consumer just in case of removing a record
-
-            // I should somehow give it some time to poll the records before I give up either because it takes too much time
-            // or the record simply does not exist
-            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
-
             System.out.println("Word to remove to " + topicName + " topic: ");
             input = reader.readLine();
-            // also, I could maybe have an option to give a list?
-            for (var consumerRecord : consumerRecords) {
-                if (consumerRecord.key().equals(input)) {
-                    ProducerRecord<String, String> record = new ProducerRecord<>(topicName, input, null);
-                    producer.send(record);
-                    System.out.println("Word removed!");
-                    break;
-                }
-            }
+            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, input, null);
+            producer.send(record);
         }
+        producer.flush();
         producer.close();
     }
 }
