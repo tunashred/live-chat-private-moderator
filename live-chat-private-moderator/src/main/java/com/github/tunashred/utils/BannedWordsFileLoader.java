b9a3848d3170728181f0d56830f749f36426605c
tunashred <enachealexandru.traian@gmail.com>
2025-03-03 23:26:23 +0200

    Add helper function to save topic words into a file

--- live-chat-private-moderator/src/main/java/com/github/tunashred/utils/BannedWordsFileLoader.java
+++ live-chat-private-moderator/src/main/java/com/github/tunashred/utils/BannedWordsFileLoader.java
@@ -1,37 +1,54 @@
 package com.github.tunashred.utils;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
+import java.io.*;
+import java.time.Duration;
+import java.util.*;
 
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 public class BannedWordsFileLoader {
     private final KafkaProducer<String, String> producer;
+    private final KafkaConsumer<String, String> consumer;
     private final String topic;
 
     public BannedWordsFileLoader(String topic) {
         this.topic = topic;
-        Properties props = new Properties();
-        props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
-        props.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ACKS_CONFIG, "all");
+        // TODO: should I move these to a .properties file?
+        Properties producerProps = new Properties();
+        producerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        producerProps.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.put(ACKS_CONFIG, "all");
+        this.producer = new KafkaProducer<>(producerProps);
 
-        this.producer = new KafkaProducer<>(props);
+        Properties consumerProps = new Properties();
+        consumerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        consumerProps.put(GROUP_ID_CONFIG, "words-manager");
+        consumerProps.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProps.put(ACKS_CONFIG, "all");
+        this.consumer = new KafkaConsumer<>(consumerProps);
     }
 
     public static void main(String[] args) {
-        BannedWordsFileLoader producer = new BannedWordsFileLoader("banned-words");
-        producer.sendWordsFromFile("packs/banned.txt");
-        producer.close();
+        BannedWordsFileLoader loader = new BannedWordsFileLoader("banned-words");
+//        loader.sendWordsFromFile("packs/banned.txt");
 
-        System.out.println("Words loaded.");
+        loader.saveWordsToFile("packs/banned-words-backup.txt");
+
+        loader.close();
+        System.out.println("Job's done");
     }
 
     public void sendWordsFromFile(String filePath) {
@@ -45,10 +62,41 @@ public class BannedWordsFileLoader {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Words loaded.");
+        }
+    }
+
+    // TODO: for future, this code must be more robust and cleaner
+    public void saveWordsToFile(String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath), "utf-8"))) {
+            List<String> wordsList = new ArrayList<>();
+            consumer.subscribe(Collections.singletonList("banned-words"));
+
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(6000));
+                if (consumerRecords.isEmpty()) {
+                    System.out.println("Done consuming records.");
+                    break;
+                }
+                for (var record : consumerRecords) {
+                    System.out.println("Consumed record key: " + record.key());
+                    wordsList.add(record.key());
+                }
+            }
+            consumer.close();
+
+            for (String word : wordsList) {
+                writer.write(word);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void close() {
+        consumer.close();
         producer.close();
     }
 }
